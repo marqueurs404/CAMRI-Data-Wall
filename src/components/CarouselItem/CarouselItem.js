b8531f85909d787a89a2b0ea6225e80076626b4a
Marcus Ong <marcus.ong404@gmail.com>
2018-10-03 17:45:24 +0800

    trying out Victory charts

--- src/components/CarouselItem/CarouselItem.js
+++ src/components/CarouselItem/CarouselItem.js
@@ -1,9 +1,14 @@
 //Item for Carousel in Commodities
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 
 class CarouselItem extends Component {
   render() {
-    return <div className="carousel-item">{this.props.children}</div>;
+    return (
+      <Row className="carousel-item" type="flex" justify="center">
+        <Col span={16}>{this.props.children}</Col>
+      </Row>
+    );
   }
 }
 
--- src/components/MultiFactorGraph/MultiFactorGraph.js
+++ src/components/MultiFactorGraph/MultiFactorGraph.js
@@ -1,46 +1,53 @@
 //Graph for Multi-Factor model
 import React, { Component } from 'react';
 import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip
-} from 'recharts';
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryAxis
+} from 'victory';
 
 import './MultiFactorGraph.css';
 
 class MultiFactorGraph extends Component {
   render() {
-    let data = [
-      { date: '11/9/17', uv: 4000, pv: 2400, amt: 2400 },
-      { date: '11/10/17', uv: 3000, pv: 1398, amt: 2210 },
-      { date: '11/11/17', uv: 2000, pv: 9800, amt: 2290 },
-      { date: '11/12/17', uv: 2780, pv: 3908, amt: 2000 },
-      { date: '11/1/18', uv: 1890, pv: 4800, amt: 2181 },
-      { date: '11/2/18', uv: 2390, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 3390, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 2390, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 1390, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 2690, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 2190, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 4390, pv: 3800, amt: 2500 },
-      { date: '11/2/18', uv: 5390, pv: 3800, amt: 2500 },
-      { date: '11/3/18', uv: 3490, pv: 4300, amt: 2100 }
+    const data = [
+      { date: '11/9/17', price: 4000, label: 4000, type: 'profitability' },
+      { date: '11/10/17', price: 3000, label: 3000, type: 'profitability' },
+      { date: '11/11/17', price: 2000, label: 2000, type: 'profitability' },
+      { date: '11/12/17', price: 2780, label: 2780, type: 'profitability' },
+      { date: '11/1/18', price: 1890, label: 1890, type: 'profitability' },
+      { date: '11/2/18', price: 2390, label: 2390, type: 'profitability' },
+      { date: '11/3/18', price: 3390, label: 3390, type: 'profitability' },
+      { date: '11/4/18', price: 2390, label: 2390, type: 'profitability' },
+      { date: '11/5/18', price: 1390, label: 1390, type: 'profitability' },
+      { date: '11/6/18', price: 2690, label: 2690, type: 'profitability' },
+      { date: '11/7/18', price: 2190, label: 2190, type: 'profitability' },
+      { date: '11/8/18', price: 4390, label: 4390, type: 'profitability' },
+      { date: '11/9/18', price: 5390, label: 5390, type: 'profitability' },
+      { date: '11/10/18', price: 3490, label: 3490, type: 'profitability' }
     ];
     return (
-      <AreaChart
-        width={850}
-        height={550}
-        data={data}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+      <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+        <VictoryAxis
+          // tickValues specifies both the number of ticks and where
+          // they are placed on the axis
+          tickValues={['11/9/17', '11/12/17', '11/3/18', '11/6/18', '11/9/18']}
+          tickFormat={['Q3/17', 'Q4/17', 'Q1/18', 'Q2/18', 'Q3/18']}
+        />
+        <VictoryAxis
+          dependentAxis
+          // tickFormat specifies how ticks should be displayed
+          tickFormat={x => `$${x}`}
+        />
+        <VictoryBar
+          labelComponent={<VictoryTooltip activateData={true} />}
+          data={data}
+          x="date"
+          y="price"
+        />
+      </VictoryChart>
     );
   }
 }
